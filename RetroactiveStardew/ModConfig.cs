85029b9ba029a15dfe357317ab6110e6d9a87f35
Marcos Miller Dantas <mmillerdantas@outlook.com>
2020-10-01 03:31:17 -0300

    Initial release
    - Implemented retroactively unlock achievements.
    - Implemented retroactively unlock recipemails.
    - Implemented retroactively special items (only works with dark talisman )

---
+++ RetroactiveStardew/ModConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace RetroactiveStardew
+{
+    public class ModConfig
+    {
+        public bool OnlyLog { get; set; } = false;
+        public bool AchievementsCheck { get; set; } = true;
+        public bool SpecialsCheck { get; set; } = true;
+        public bool RetroMail { get; set; } = true;
+    }
+}
